b48731ce9ac45e4798eba62a630efea46f97ca99
fronexxx <igorbotsii42@gmail.com>
2026-04-07 19:51:37 +0300

    homeworks/hw-2

--- apps/pizza/filter.py
+++ apps/pizza/filter.py
@@ -3,6 +3,7 @@ from django.http import QueryDict
 from rest_framework.exceptions import ValidationError
 
 from apps.pizza.models import PizzaModel
+from apps.pizza.serializer import PizzaSerializer
 
 
 def filter_pizza(query: QueryDict) -> QuerySet:
@@ -14,6 +15,32 @@ def filter_pizza(query: QueryDict) -> QuerySet:
                 qs = qs.filter(price__gt=v)
             case 'price_lt':
                 qs = qs.filter(price__lt=v)
+            case 'price_gte':
+                qs = qs.filter(price__gte=v)
+            case 'price_lte':
+                qs = qs.filter(price__lte=v)
+            case 'size_gt':
+                qs = qs.filter(size__gt=v)
+            case 'size_lt':
+                qs = qs.filter(size__lt=v)
+            case 'size_gte':
+                qs = qs.filter(size__gte=v)
+            case 'size_lte':
+                qs = qs.filter(size__lte=v)
+            case 'name_starts':
+                qs = qs.filter(name__startswith=v)
+            case 'name_ends':
+                qs = qs.filter(name__endswith=v)
+            case 'name_contains':
+                qs = qs.filter(name__contains=v)
+            case 'order':
+                fields = PizzaSerializer.Meta.fields
+                allowed_fields = (*fields, *[f'-{field}' for field in fields])
+
+                if v not in allowed_fields:
+                    raise ValidationError({'details': f'allowed fields are as follows: {allowed_fields}'})
+
+                qs = qs.order_by(v)
             case _:
                 raise ValidationError({'details': f'{k} not allowed'})
 
