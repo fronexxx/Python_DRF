9526e1a92a109130f69693527667c6ecc2fb1aa9
fronexxx <igorbotsii42@gmail.com>
2026-04-14 21:40:14 +0300

    homeworks/hw-4

--- apps/pizzas/filter.py
+++ apps/pizzas/filter.py
@@ -1,68 +1,23 @@
-from random import choices
-
-from django.db.models import QuerySet
-from django.http import QueryDict
-
-from rest_framework.exceptions import ValidationError
-
 from django_filters import rest_framework as filters
 
-from apps.pizzas.models import DaysChoices
 from apps.pizzas.serializer import PizzaSerializer
-
-# def pizza_filter(query: QueryDict) -> QuerySet:
-#     qs = PizzaModel.objects.all()
-#
-#     for k, v in query.items():
-#         match k:
-#             case 'price_gt':
-#                 qs = qs.filter(price__gt=v)
-#             case 'price_lt':
-#                 qs = qs.filter(price__lt=v)
-#             case 'price_gte':
-#                 qs = qs.filter(price__gte=v)
-#             case 'price_lte':
-#                 qs = qs.filter(price__lte=v)
-#             case 'size_gt':
-#                 qs = qs.filter(size__gt=v)
-#             case 'size_lt':
-#                 qs = qs.filter(size__lt=v)
-#             case 'size_gte':
-#                 qs = qs.filter(size__gte=v)
-#             case 'size_lte':
-#                 qs = qs.filter(size__lte=v)
-#             case 'name_starts':
-#                 qs = qs.filter(name__startswith=v)
-#             case 'name_ends':
-#                 qs = qs.filter(name__endswith=v)
-#             case 'name_contains':
-#                 qs = qs.filter(name__contains=v)
-#             case 'order':
-#                 fields = PizzaSerializer.Meta.fields
-#                 allowed_fields = (*fields, *[f'-{field}' for field in fields])
-#
-#                 if v not in allowed_fields:
-#                     raise ValidationError({'details': f'allowed fields is as follows: {allowed_fields}'})
-#
-#                 qs = qs.order_by(v)
-#
-#             case _:
-#                 raise ValidationError({'details': f'{k} does not exist'})
-#
-#     return qs
-
 
 
 class PizzaFilter(filters.FilterSet):
-    lt = filters.NumberFilter(field_name='price', lookup_expr='lt')
-    range = filters.RangeFilter(field_name='size') # range_min=2&range_max=100
-    price_in = filters.BaseInFilter(field_name='price') # price_in=30,25,2000
-    day = filters.ChoiceFilter('day', choices=DaysChoices.choices)
+    name_starts_with = filters.CharFilter(field_name='name', lookup_expr='startswith')
+    name_ends_with = filters.CharFilter(field_name='name', lookup_expr='endswith')
+    name_contains = filters.CharFilter(field_name='name', lookup_expr='contains')
+    day_starts_with = filters.CharFilter(field_name='day', lookup_expr='startswith')
+    day_ends_with = filters.CharFilter(field_name='day', lookup_expr='endswith')
+    day_contains = filters.CharFilter(field_name='day', lookup_expr='contains')
+    size_gt = filters.NumberFilter(field_name='size', lookup_expr='gt')
+    size_gte = filters.NumberFilter(field_name='size', lookup_expr='gte')
+    size_lt = filters.NumberFilter(field_name='size', lookup_expr='lt')
+    size_lte = filters.NumberFilter(field_name='size', lookup_expr='lte')
+    price_gt = filters.NumberFilter(field_name='price', lookup_expr='gt')
+    price_lt = filters.NumberFilter(field_name='price', lookup_expr='lt')
+    price_gte = filters.NumberFilter(field_name='price', lookup_expr='gte')
+    price_lte = filters.NumberFilter(field_name='price', lookup_expr='lte')
     order = filters.OrderingFilter(
-        fields=(
-            'id',
-            'name',
-            ('price', 'asd'),
-        )
-    ) # order=name asc
-      # order=-name desc
+        fields = PizzaSerializer.Meta.fields
+    )
