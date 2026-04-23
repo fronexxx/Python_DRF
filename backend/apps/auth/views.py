b2f6271d049008eff86df3d6cce22b7b929ea538
fronexxx <igorbotsii42@gmail.com>
2026-04-23 18:49:34 +0300

    homeworks/hw-7

--- backend/apps/auth/views.py
+++ backend/apps/auth/views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from core.services.jwt_service import ActivateToken, JWTService
+from core.services.email_service import EmailService
+from core.services.jwt_service import ActivateToken, JWTService, RecoveryToken
 
+from apps.auth.serializer import EmailSerializer, PasswordSerializer
 from apps.user.serializers import UserSerializer
 
+UserModel = get_user_model()
 
 class ActivateUserView(GenericAPIView):
     permission_classes = (AllowAny,)
@@ -19,3 +24,28 @@ class ActivateUserView(GenericAPIView):
         serializer = UserSerializer(user)
         return Response(serializer.data, status.HTTP_200_OK)
 
+
+class RecoveryRequestView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    def post(self, *args, **kwargs):
+        data = self.request.data
+        serializer = EmailSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        user = get_object_or_404(UserModel, email=serializer.data['email'])
+        EmailService.recovery(user)
+        return Response({'details': 'link sent to email'}, status.HTTP_200_OK)
+
+
+
+class RecoveryPasswordView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    def post(self, *args, **kwargs):
+        data = self.request.data
+        serializer = PasswordSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        token = kwargs['token']
+        user = JWTService.verify_token(token, RecoveryToken)
+        user.set_password(serializer.data['password'])
+        user.save()
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK)
