43dc98636d722e3fd2a76ea93a5b6afcdf90bc0e
fronexxx <igorbotsii42@gmail.com>
2026-04-29 19:14:45 +0300

    homeworks/hw-9

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -1,8 +1,54 @@
+from channels.db import database_sync_to_async
+from djangochannelsrestframework.decorators import action
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
 
 
 class ChatConsumer(GenericAsyncAPIConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.room_name = None
+        self.user_name = None
+
     async def connect(self):
         if not self.scope['user']:
             return await self.close()
-        return await super().connect()
+
+        await self.accept()
+
+        self.room_name = self.scope['url_route']['kwargs']['room']
+        self.user_name = await self.get_profile_name()
+
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                'type': 'sender',
+                'message': f'{self.user_name} connected to chat'
+            }
+        )
+
+    async def sender(self, data):
+        print(data)
+        await self.send_json(data)
+
+    @action()
+    async def send_message(self, data, request_id, action):
+        print(action)
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                'type': 'sender',
+                'message': data,
+                'user': self.user_name,
+                'id': request_id
+            }
+        )
+
+    @database_sync_to_async
+    def get_profile_name(self):
+        user = self.scope['user']
+        return user.profile.name
