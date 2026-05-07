566849981ca91752af6c240d9aa95fa91dad234c
fronexxx <igorbotsii42@gmail.com>
2026-05-07 20:00:53 +0300

    lessons/lesson12

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -1,6 +1,8 @@
 import datetime
 
+from django.contrib.auth import get_user_model
 from django.db.models.expressions import F
+from django.db.models.query_utils import Q
 
 from channels.db import database_sync_to_async
 from djangochannelsrestframework.decorators import action
@@ -8,6 +10,7 @@ from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
 
 from apps.chat.models import ChatRoomModel, MessageModel
 
+UserModel = get_user_model()
 
 class ChatConsumer(GenericAsyncAPIConsumer):
     def __init__(self, *args, **kwargs):
@@ -23,8 +26,12 @@ class ChatConsumer(GenericAsyncAPIConsumer):
 
         room_name = self.scope['url_route']['kwargs']['room']
         self.room, _ = await ChatRoomModel.objects.aget_or_create(name=room_name)
-
         self.user_name = await self.get_profile_name()
+
+        await self.channel_layer.group_add(
+            f'user_{self.scope['user'].id}',
+            self.channel_name
+        )
 
         await self.channel_layer.group_add(
             self.room.name,
@@ -32,10 +39,10 @@ class ChatConsumer(GenericAsyncAPIConsumer):
         )
 
         messages = await self.get_last_five_messages()
-        for text, name in messages:
+        for name, text in messages:
             await self.sender({
                 'message': text,
-                'user': name,
+                'user': f'{name}',
                 'request_id': str(datetime.datetime.now())
             })
 
@@ -43,7 +50,7 @@ class ChatConsumer(GenericAsyncAPIConsumer):
             self.room.name,
             {
                 'type': 'sender',
-                'message': f'{self.user_name} connected to chat'
+                'message': f'{self.scope['user'].id}_{self.user_name} connected to chat'
             }
         )
 
@@ -53,13 +60,34 @@ class ChatConsumer(GenericAsyncAPIConsumer):
 
     @action()
     async def send_message(self, data, request_id, action):
-        await MessageModel.objects.acreate(room=self.room, user=self.scope['user'], text=data)
+        await MessageModel.objects.acreate(room=self.room, user=self.scope['user'], text=data['text'])
         await self.channel_layer.group_send(
             self.room.name,
             {
                 'type': 'sender',
-                'message': data,
-                'user': self.user_name,
+                'message': data['text'],
+                'user': f'{self.scope['user'].id}_{self.user_name}',
+                'id': request_id
+            }
+        )
+
+    @action()
+    async def send_private_message(self, data, request_id, action):
+        print(data)
+        self.private_room_name = f'user_{data['userId']}'
+        private_room, is_created = await ChatRoomModel.objects.aget_or_create(name=self.private_room_name, is_private=True)
+        await private_room.users.aadd(await UserModel.objects.aget(pk=data['userId']), self.scope['user'])
+        await MessageModel.objects.acreate(room=private_room, user=self.scope['user'], text=data['text'])
+        await self.channel_layer.group_add(
+            self.private_room_name,
+            self.channel_name
+        )
+        await self.channel_layer.group_send(
+            self.private_room_name,
+            {
+                'type': 'sender',
+                'message': data['text'],
+                'user': f'{self.scope['user'].id}_{self.user_name}',
                 'id': request_id
             }
         )
@@ -71,7 +99,10 @@ class ChatConsumer(GenericAsyncAPIConsumer):
 
     @database_sync_to_async
     def get_last_five_messages(self):
-        res = self.room.messages.annotate(name=F('user__profile__name')).values('text', 'name').order_by('-id')[:5]
-        return reversed(([(msg['name'], msg['text']) for msg in res]))
-
-
+        # res = self.room.messages.annotate(name=F('user__profile__name'), pk=F('user__pk')).values('text', 'name', 'pk').order_by('-id')[:5]
+        res = MessageModel.objects.filter(
+            Q(room=self.room) | (Q(room__is_private=True) & Q(room__users__in=[self.scope['user']]))
+        ).annotate(
+            name=F('user__profile__name'), pk=F('user__pk')
+        ).values('text', 'name', 'pk').order_by('-id')[:5]
+        return reversed(([(f'{msg['pk']}_{msg['name']}', msg['text']) for msg in res]))
